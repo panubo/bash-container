7adc418bfe910b6070a3f8b0b9fac8b2a45ce00c
Andrew Cutler <andrew@panubo.com>
2017-07-04 15:52:01 +1000

    Initial add

---
+++ functions/wait_rabbitmq.sh
@@ -0,0 +1,20 @@
+function wait_rabbitmq {
+    # LICENSE: MIT License, Copyright (c) 2017 Volt Grid Pty Ltd
+    # Wait for RabbitMQ to be available
+    local host=${1:-'localhost'}
+    local port=${2:-'5672'}
+    local timeout=${3:-30}
+    echo -n "Waiting to connect to RabbitMQ at ${host}:${port}"
+    for (( i=0;; i++ )); do
+        if [ ${i} -eq ${timeout} ]; then
+            echo " timeout!"
+            exit 99
+        fi
+        sleep 1
+        (exec 3<>/dev/tcp/${host}/${port}) &>/dev/null && break
+        echo -n "."
+    done
+    echo " connected."
+    exec 3>&-
+    exec 3<&-
+}
