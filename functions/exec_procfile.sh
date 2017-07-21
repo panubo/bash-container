24302bce176d0c72f8f75a8b31545fa485211978
Andrew Cutler <andrew@panubo.com>
2017-07-21 14:55:10 +1000

    Remove Deployfile hook

--- functions/exec_procfile.sh
+++ functions/exec_procfile.sh
@@ -1,14 +1,11 @@
 
-run_procfile() {
+exec_procfile() {
     # LICENSE: MIT License, Copyright (c) 2017 Volt Grid Pty Ltd
-    # Run Procfile commands
+    # Exec Procfile command
 	local procfile=${1:-'Procfile'}
 	while read line || [[ -n "$line" ]]; do
 		if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
 		if [[ "${2}" == "${line%%:*}" ]]; then
-			if [ "x${NO_DEPLOY}" == "x" ]; then
-				(run_deploy "Deployfile")
-			fi
 			eval exec "${line#*:[[:space:]]}"
 		fi
 	done < "$procfile"
