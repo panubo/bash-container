75343b5f3966b1d0c3b7b084f4b9dd742a3a9dd2
Andrew Cutler <andrew@panubo.com>
2017-12-08 12:04:17 +1100

    add check for procfile, fix tabs

--- functions/exec_procfile.sh
+++ functions/exec_procfile.sh
@@ -2,12 +2,13 @@
 exec_procfile() {
     # LICENSE: MIT License, Copyright (c) 2017 Volt Grid Pty Ltd
     # Exec Procfile command
-	local procfile=${1:-'Procfile'}
-	while read line || [[ -n "$line" ]]; do
-		if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
-		if [[ "${2}" == "${line%%:*}" ]]; then
-			echo "Executing ${2} from ${1}..."
-			eval exec "${line#*:[[:space:]]}"
-		fi
-	done < "$procfile"
+    local procfile=${1:-'Procfile'}
+    if [ ! -e "$procfile" ]; then return 0; fi
+    while read line || [[ -n "$line" ]]; do
+        if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
+        if [[ "${2}" == "${line%%:*}" ]]; then
+            echo "Executing ${2} from ${1}..."
+            eval exec "${line#*:[[:space:]]}"
+        fi
+    done < "$procfile"
 }
