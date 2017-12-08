abfc3c4b2a7a8510f7b175e5ed07cba5929e5dd1
Andrew Cutler <andrew@panubo.com>
2017-12-08 12:04:59 +1100

    Initial add

---
+++ functions/run_deployfile_commands.sh
@@ -0,0 +1,20 @@
+
+run_deployfile_commands() {
+    # LICENSE: MIT License, Copyright (c) 2017 Volt Grid Pty Ltd
+    # Run specified Deployfile commands
+    local deployfile=${1:-'Deployfile'}
+    shift
+    if [ ! -e "$deployfile" ]; then return 0; fi
+    for command_name in "$@"; do
+        while read line || [[ -n "$line" ]]; do
+            if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
+            if [[ "${command_name}" == "${line%%:*}" ]]; then
+                (>&2 echo "Running task ${line%%:*}: ${line#*:[[:space:]]}")
+                eval "${line#*:[[:space:]]}"
+                rc=$?
+                [ "$rc" != 0 ] && exit $rc
+            fi
+        done < "$deployfile"
+    done
+    exit 0
+}
