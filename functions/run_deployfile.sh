8b18842d6ed53e26846bc29a1b4f4cfe30563ebb
Andrew Cutler <andrew@panubo.com>
2017-12-08 12:04:40 +1100

    fix tabs

--- functions/run_deployfile.sh
+++ functions/run_deployfile.sh
@@ -1,12 +1,12 @@
 
 run_deployfile() {
     # LICENSE: MIT License, Copyright (c) 2017 Volt Grid Pty Ltd
-    # Run Deployfile commands
-	local deployfile=${1:-'Deployfile'}
-	if [ ! -e "$deployfile" ]; then return 0; fi
-	while read line || [[ -n "$line" ]]; do
-		if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
-		(>&2 echo "Running task ${line%%:*}: ${line#*:[[:space:]]}")
-		eval "${line#*:[[:space:]]}"
-	done < "$deployfile"
+    # Run all Deployfile commands
+    local deployfile=${1:-'Deployfile'}
+    if [ ! -e "$deployfile" ]; then return 0; fi
+    while read line || [[ -n "$line" ]]; do
+        if [[ -z "$line" ]] || [[ "$line" == \#* ]]; then continue; fi
+        (>&2 echo "Running task ${line%%:*}: ${line#*:[[:space:]]}")
+        eval "${line#*:[[:space:]]}"
+    done < "$deployfile"
 }
