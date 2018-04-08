4ced5a47323a42774602ae45863bf8599f761dd5
Tim Robinson <tim@panubo.com>
2018-04-08 19:50:19 +1000

    Add import_env function

---
+++ functions/import_env.sh
@@ -0,0 +1,19 @@
+# Docker --env-file doesn't interpret env files the the same as bash (`. ./env`) does 
+# eg. 'VARIABLE="foo bar"' would be 'VARIABLE=foo bar' in bash but 'VARIABLE="foo bar"' with Docker
+# This function sources and env file and then exports each variable by name (avoiding issues trying to do export $(cat env))
+# Since this is executed as a function the exports will be available to sub processes.
+# Usage: import_env ENVFILE
+import_env() {
+  local file
+  [[ ! -e "${1}" ]] && error "${1} does not exist"
+  [[ ! -f "${1}" ]] && error "${1} is not a file"
+  file="$(realpath "${1}")"
+  # shellcheck disable=SC1090
+  . "${file}"
+
+  while read -r item; do
+  	# Disabling shellcheck warning see https://github.com/koalaman/shellcheck/wiki/SC2163
+  	# shellcheck disable=SC2163
+    export "${item?}"
+  done < <(sed -E -e '/^(#|$)/d' -e 's/([a-zA-Z0-9_])=.*/\1/' "${file}")
+}
---
+++ tests/import_env.bats
@@ -0,0 +1,28 @@
+#!/usr/bin/env bats
+
+# It is difficult to test the action of this function so we're just checking it
+# runs without error and fails on common issues.
+
+source ../functions/10-common.sh
+source ../functions/import_env.sh
+
+teardown() {
+  rm -rf env 2>/dev/null || true
+}
+
+@test "import_env: run successful" {
+  echo "VARIABLE=value" > env
+  run import_env env
+  [ "$status" -eq 0 ]
+}
+
+@test "import_env: error on missing file" {
+  run import_env env
+  [ "$status" -eq 1 ]
+}
+
+@test "import_env: error on directory instead of file" {
+  mkdir env
+  run import_env env
+  [ "$status" -eq 1 ]
+}
