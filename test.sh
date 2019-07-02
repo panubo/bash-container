7657573e387420733a9477bed8bf85ea8506fc79
Andrew Cutler <andrew@panubo.com>
2019-07-02 12:14:03 +1000

    log verion of bats, and fix requirement test

--- test.sh
+++ test.sh
@@ -3,16 +3,19 @@
 set -e
 
 # Check for requirements
-for C in 'bats curl gomplate timeout'; do
+for C in bats curl gomplate timeout; do
   command -v ${C} >/dev/null 2>&1 || { echo "Error: Tests require ${C} to be installed."; exit 1; }
 done
 
-# Run all bats tests
+# Log version of bats used
+bats --version
 
+# trap errors
 finish() {
   popd 1> /dev/null
 }
 trap finish EXIT
 
+# Run all bats tests
 pushd tests 1> /dev/null
 bats *.bats
