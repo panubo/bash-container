28310236ff7865741e008089e1391b9934aa4c5f
Andrew Cutler <andrew@panubo.com>
2022-03-30 13:58:15 +1100

    Increment year

--- functions/00-LICENSE.sh
+++ functions/00-LICENSE.sh
@@ -1,3 +1,3 @@
 # shellcheck shell=bash disable=SC2148
-# LICENSE: MIT License, Copyright (c) 2017-2021 Volt Grid Pty Ltd t/a Panubo
+# LICENSE: MIT License, Copyright (c) 2017-2022 Volt Grid Pty Ltd t/a Panubo
 # See: https://github.com/panubo/bash-container
