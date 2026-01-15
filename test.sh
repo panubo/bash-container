d959b4de41a73cf43ce69a345c2a183d6b0bb801
Andrew Cutler <andrew@panubo.com>
2026-01-15 17:23:23 +1100

    Fix test.sh test globbing

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo ">> Running ${0}..."
+
 # Check for requirements
 for C in bats curl gomplate timeout; do
   command -v ${C} >/dev/null 2>&1 || { echo "Error: Tests require ${C} to be installed."; exit 1; }
@@ -18,4 +20,10 @@ trap finish EXIT
 
 # Run all bats tests
 pushd tests 1> /dev/null
-bats "${1:-*}".bats
+if [ -z "$1" ]; then
+  for f in *.bats; do
+    bats "$f"
+  done
+else
+  bats "$1".bats
+fi
